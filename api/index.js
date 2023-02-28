315ca51f5375140cb74a84dc6e6e55163a6132e3
DESKTOP-1KBC29H\Super Power Station <paul@mind-alliance.com>
2023-03-01 00:59:46 +0200

    API: Search and update endpoints added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const ParseServer = require('parse-server').ParseServer;
 const path = require('path');
 const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
+const cors = require('cors');
+const Parse = require('parse/node');
+const bodyParser = require('body-parser')
 require('dotenv').config();
 
 const databaseUri = process.env.DATABASE_URI;
@@ -10,7 +13,13 @@ const databaseUri = process.env.DATABASE_URI;
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
+
+// https://parseplatform.org/parse-server/api/5.0.0/ParseServerOptions.html
 const config = {
+  fileUpload: {
+    enableForAuthenticatedUser: true,
+    enableForPublic: true
+  },
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
@@ -18,35 +27,104 @@ const config = {
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', 
   liveQuery: {
     classNames: ['Landmarks'], 
-  },
+  }
 };
 
 const app = express();
 
+Parse.initialize(process.env.APP_ID, process.env.MASTER_KEY);
+Parse.serverURL = process.env.SERVER_URL;
+
+// enabling CORS for any unknown origin
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json())
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/parse';
+
 if (!test) {
   const api = new ParseServer(config);
+
   app.use(mountPath, api);
+  
+  // app.get('/parse/landmarks', async (req, res) => {
+  //   let landmarks = [];
+  //   const Landmark = Parse.Object.extend("Landmark");
+  //   const query = new Parse.Query(Landmark)
+  //                          .select(['title','photo','photo_thumb','short_info','objectId'])
+  //                          .descending("order"); 
+
+  //   try {
+  //     landmarks = await query.find();
+  //   } catch(error) {
+  //     console.log(error);
+  //   }
+    
+  //   res.status(200).send(landmarks);
+  // });
+  
+  // Get specific landmark object
+  // app.get('/parse/landmarks/:id', async (req, res) => {
+  //   let landmark;
+  //   const { id } = req.params;
+  //   const Landmark = Parse.Object.extend("Landmark");
+  //   const query = new Parse.Query(Landmark); 
+  //   query.get(id);
+
+  //   try {
+  //     landmark = await query.first();
+  //   } catch(error) {
+  //     console.log(error);
+  //   }
+    
+  //   res.status(200).send(landmark);
+  // });
+  
+  // Update landmark object
+  app.put('/landmarks/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, url, short_info, description } = req.body;
+  
+    let landmark;
+    const Landmark = Parse.Object.extend("Landmark");
+    const query = new Parse.Query(Landmark);
+    await query.get(id);
+    landmark = await query.first();
+  
+    try {
+      landmark.set("title", title);
+      landmark.set("description", description);
+      landmark.set("short_info", short_info);
+      landmark.set("url", url);
+      await landmark.save();
+    } catch(error) {
+      console.log(error);
+    }
+  
+    res.status(200).send(landmark);
+  });
+
+  app.get('/landmarks/search', async (req, res) => {
+    const { text } = req.query;
+    
+    let landmarks;
+    const Landmark = Parse.Object.extend("Landmark");
+    const query = new Parse.Query(Landmark);
+    query.matches('title', new RegExp(text, 'i'));
+    landmarks = await query.find();
+  
+    res.status(200).send(landmarks);
+  });
 }
-
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function (req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
-
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
 
 const port = process.env.SERVER_PORT || 1337;
 if (!test) {
   const httpServer = require('http').createServer(app);
   httpServer.listen(port, function () {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('Running on port ' + port + '.');
   });
   // This will enable the Live Query real-time server
   ParseServer.createLiveQueryServer(httpServer);
